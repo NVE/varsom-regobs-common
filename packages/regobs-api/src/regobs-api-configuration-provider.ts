fb9dc50d8355c715f89df5f88c60f898c3408725
ingljo <ingljo@gmail.com>
2020-07-27 15:38:06 +0200

    Updated to angular 10

--- packages/regobs-api/src/regobs-api-configuration-provider.ts
+++ packages/regobs-api/src/regobs-api-configuration-provider.ts
@@ -1,9 +1,10 @@
 import { AppModeService, AppMode } from '@varsom-regobs-common/core';
 import { RegobsApiConfigurationInterface } from './regobs-api-configuration';
-import { OnDestroy } from '@angular/core';
+import { OnDestroy, Injectable } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { settings } from './settings';
 
+@Injectable()
 export class RegObsApiConfigurationProvider implements RegobsApiConfigurationInterface, OnDestroy {
     private subscription: Subscription;
     private appMode: AppMode = AppMode.Prod;
